c4eedc1e1eb64b34b3882f81e4a532dcaa5eb4f1
Slazer22 <48864818+Slazer22@users.noreply.github.com>
2022-05-18 12:11:26 -0500

    Add project files.

---
+++ What Day is it/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+
+namespace Weekdays;
+
+class ExerciseTwo
+{
+    public static void Main(string[] args)
+    {
+        char letter;
+        Console.WriteLine("Please enter a letter");
+        letter = Convert.ToChar(Console.ReadLine());
+
+        switch(letter)
+        {
+            case 'A':
+                Console.WriteLine("Today is Monday");
+                break;
+
+            case 'B':
+                Console.WriteLine("Today is Tuesday");
+                break;
+
+            case 'C':
+                Console.WriteLine("Today is Wednesday");
+                break;
+
+            case 'D':
+                Console.WriteLine("Today is Thursday");
+                break;
+
+            case 'E':
+                Console.WriteLine("Today is Friday! Wooohoooo");
+                break;
+
+            case 'F':
+                Console.WriteLine("Today is Saturday! Good i can work out now");
+                break;
+
+            case 'G':
+                Console.WriteLine("Today is Sunday! Time to go worship the lord");
+                break;
+
+
+            default:
+                Console.WriteLine("Try again. Out of the Domain");
+                break;
+
+
+
+            
+        }
+    }
+}
+
